762551885f513ed49ad3b07f7e5a7c37444e0d8f
BlainedZA <blainedza@gmail.com>
2020-03-23 23:51:18 +0200

    Add empty project

---
+++ StartingGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    bool gameHasEnded = false;
+
+    public AudioSource deathSound;
+
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+
+    void Start()
+    {
+        deathSound = GetComponent<AudioSource>();
+    }
+
+    public void CompleteLevel ()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            deathSound.Play();
+            Invoke("Restart", restartDelay);
+        }
+        
+    }
+
+    void Restart ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ StartingGame/Assets/Scripts/LevelComplete.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelComplete : MonoBehaviour
+
+{
+
+    public AudioSource levelUpSound;
+
+    public float levelUpDelay = 1f;
+
+    public PlayerMovement movement;
+
+
+
+    void Start()
+    {
+        levelUpSound = GetComponent<AudioSource>();
+        movement.enabled = false;
+        levelUpSound.Play();
+    }
+
+
+    public void LoadNextLevel ()
+    {
+        Invoke("LevelEnd", levelUpDelay);
+    }
+
+    void LevelEnd ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+}
